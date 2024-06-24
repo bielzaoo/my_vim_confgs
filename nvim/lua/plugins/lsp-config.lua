2b812b632d1f5b3f29dab64dd6c11e7ba472bf1f
bielzaoo <bielzao0x7@gmail.com>
2024-06-24 16:46:54 -0300

    nvim

---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,50 @@
+return {
+  {
+    "williamboman/mason.nvim",
+  
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "tsserver",
+          "jedi_language_server",
+          "clangd",
+          "asm_lsp"
+        },
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities
+      })
+      lspconfig.jedi_language_server.setup({
+        capabilities = capabilities
+      })
+      lspconfig.clangd.setup({
+        capabilities = capabilities
+      })
+      lspconfig.asm_lsp.setup({
+        capabilities = capabilities
+      })
+  
+
+      vim.keymap.set('n','K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n','gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({'n','v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+    end
+  }
+}
